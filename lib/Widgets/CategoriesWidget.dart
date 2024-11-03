6a341eea4710c133d026d6ead4bbad574436d38f
AzzahraCitraRiyanti <zaracitrariyanti@gmail.com>
2024-11-03 10:25:53 +0700

    Update CategoriesWidget.dart

--- lib/Widgets/CategoriesWidget.dart
+++ lib/Widgets/CategoriesWidget.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CategoriesWidget extends StatelessWidget{
+class CategoriesWidget extends StatefulWidget {
+  @override
+  _CategoriesWidgetState createState() => _CategoriesWidgetState();
+}
+
+class _CategoriesWidgetState extends State<CategoriesWidget> {
+  int? selectedIndex; 
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -8,80 +15,45 @@ class CategoriesWidget extends StatelessWidget{
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
         child: Row(
-          children: [
-
-            //single item
-            // for(int i=0; i<10; i++)
-            Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Container(
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.white, 
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 10,
-                    offset: Offset(0, 3),
-                  )
-                ]),
-              child: Image.asset(
-                'images/drink.jpeg',
-                width: 80,
-                height: 80,
+          children: [          
+            for (int i = 0; i < 3; i++) 
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedIndex = i; 
+                    });
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: selectedIndex == i ? Colors.blue[100] : Colors.white, 
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        )
+                      ],
+                    ),
+                    child: Image.asset(
+                      i == 0
+                          ? 'images/drink.jpeg'
+                          : i == 1
+                              ? 'images/burger.jpeg'
+                              : 'images/ice cream.jpeg',
+                      width: 80,
+                      height: 80,
+                    ),
+                  ),
                 ),
-            ),
-            ),
-
-            Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Container(
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.white, 
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 10,
-                    offset: Offset(0, 3),
-                  )
-                ]),
-              child: Image.asset(
-                "images/burger.jpeg",
-                width: 80,
-                height: 80,
-                ),
-            ),
-            ),
-
-            Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Container(
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.white, 
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 10,
-                    offset: Offset(0, 3),
-                  )
-                ]),
-              child: Image.asset(
-                "images/ice cream.jpeg",
-                width: 80,
-                height: 80,
-                ),
-            ),
-            ),
-        ],)
-      )
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
